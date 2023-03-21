521cc5ad7929028093e8f4dcd417b890c2fc2bd2
Precious Juwah <100835674+Feisty007@users.noreply.github.com>
2023-03-21 11:36:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Wait for the page to load before executing our code
 $(document).ready(function() {
+  $('#loader').hide('slow');
+
   // Initialize the month to the current month
   var currentMonth = new Date().getMonth() + 1;
   // Get the JWT from the API and store it in a variable
@@ -15,8 +17,12 @@ $(document).ready(function() {
       loadEvents(jwt, currentMonth);
       // When the user selects a different month, reload the events for that month
       $('#month-select').change(function() {
+        $('#loader').show('slow');
         var month = $(this).val();
-        loadEvents(jwt, month);
+        setTimeout(function() {
+          loadEvents(jwt, month);
+        }, 2000);
+
       });
     },
     error: function(jqXHR, textStatus, errorThrown) {
@@ -26,36 +32,43 @@ $(document).ready(function() {
 });
 
 function loadEvents(jwt, month) {
+
+    // Get the events for the specified month from the API
+    $.ajax({
+      url: 'https://api.arenaracingcompany.co.uk/event/month/1318/' + month,
+      type: 'GET',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + jwt);
+      },
+      success: function(response) {
+        if(response.length > 0){
+          $.each(response, function(index, event) {
+            var id = event.id;
+            var date = new Date(event.date).toLocaleDateString();
+            var slug = event.slug;
+            var title = event.title;
+            var description = event.description;
+            var images = event.images;
+            var card = '<div class="col-lg-4 col-md-6 mb-5"><div class="card event-card p-2">';
+            if (images) {
+              card += '<div class="card-img"><img src="' + images.desktop + '" class="card-img-top" alt="' + slug + '"></div>';
+            }
+            card += '<div class="card-header"><h6>' + title + '</h6></div>'
+            card += '<div class="card-body"><p>' + description + '</p></div>'
+            card += '<div class="card-footer"><small class="text-muted">ID: ' + id + ' | Date: ' + date + '</small></div></div></div>';
+            $('#events-container').append(card);
+          });
+        }
+        // Loop through each event and append it to the container
+
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+      }
+    });
+
+
   // Clear any existing events from the container
   $('#events-container').empty();
-  // Get the events for the specified month from the API
-  $.ajax({
-    url: 'https://api.arenaracingcompany.co.uk/event/month/1318/' + month,
-    type: 'GET',
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('Authorization', 'Bearer ' + jwt);
-    },
-    success: function(response) {
-      // Loop through each event and append it to the container
-      $.each(response, function(index, event) {
-        var id = event.id;
-        var date = new Date(event.date).toLocaleDateString();
-        var slug = event.slug;
-        var title = event.title;
-        var description = event.description;
-        var images = event.images;
-        var card = '<div class="col-lg-4 col-md-6 mb-5"><div class="card event-card p-2">';
-        if (images) {
-          card += '<div class="card-img"><img src="' + images.desktop + '" class="card-img-top" alt="' + slug + '"></div>';
-        }
-        card += '<div class="card-header"><h6>' + title + '</h6></div>'
-        card += '<div class="card-body"><p>' + description + '</p></div>'
-        card += '<div class="card-footer"><small class="text-muted">ID: ' + id + ' | Date: ' + date + '</small></div></div></div>';
-        $('#events-container').append(card);
-      });
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus, errorThrown);
-    }
-  });
+$('#loader').hide('slow');
 }
